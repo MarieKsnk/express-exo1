6bc80e5f463a5f97891f17eaba576959496da946
Marie Klisnick <marie63.klisnick@gmail.com>
2025-03-04 11:56:01 +0100

    Ajout ID

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get("/actors", (req, res) => {
   res.json(actors);
 });
 
+app.get("/actors/:id", (req, res) => {
+  const actorID = req.params.id;
+  const actorByID = actors.find((actor) => actor.id == actorID);
+  if (!actorByID) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  return res.status(200).json(actorByID);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
